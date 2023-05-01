6f5ddcea45733d2f44292f166b54f79d6b40ac20
김하영 <bnk159hair@naver.com>
2023-05-01 23:17:51 +0900

    chore : pom.xml 버전 수정 및 UserDto 누락 해결

--- src/main/java/com/ssafy/enjoytrip/user/dto/UserDto.java
+++ src/main/java/com/ssafy/enjoytrip/user/dto/UserDto.java
@@ -1,2 +1,60 @@
-package com.ssafy.enjoytrip.user.dto;public class UserDto {
+package com.ssafy.enjoytrip.user.dto;
+
+public class UserDto {
+    private String userName;
+    private String userNickname;
+    private String userEmail;
+    private String userPassword;
+    private String regDate;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "userName='" + userName + '\'' +
+                ", userNickname='" + userNickname + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", regDate='" + regDate + '\'' +
+                '}';
+    }
 }
